030f747f6a23cbb90bf59605b380023c7a4dba60
Christian <christianhougaardpedersen@gmail.com>
2021-12-01 10:03:06 +0100

    Added Christians code

---
+++ Sep1/Session.java
@@ -0,0 +1,97 @@
+public class Session
+{
+  private Course course;
+  private int length;
+  private DateTime dateTime;
+  private Room room;
+
+  /**
+   * Constructor for Session
+   *
+   * @param course   the course
+   * @param length
+   * @param dateTime
+   * @param room
+   */
+  public Session(Course course, int length, DateTime dateTime, Room room)
+  {
+    this.course = course;
+
+    if (length == 2 || length == 3 || length == 4)
+    {
+      this.length = length;
+    }
+    else
+    {
+      throw new IllegalArgumentException(
+          "Length Error - length should be 2, 3 or 4");
+    }
+
+    this.dateTime = dateTime.copy();
+
+    if (course.getStudentList.size() <= room.getCapacity()
+        && !(room.isBooked()))
+    {
+      this.room = room.copy;
+
+      // This definately shouldn't work...
+      this.room.isBooked(dateTime, dateTime + (length * 45));
+    }
+    else
+    {
+      throw new IllegalArgumentException("Room to small to hold students.");
+    }
+
+  }
+
+  public Course getCourse()
+  {
+    return course;
+  }
+
+  public int getLength()
+  {
+    return length;
+  }
+
+  public DateTime getDateTime()
+  {
+    return dateTime.copy();
+  }
+
+  public Room getRoom()
+  {
+    return room.copy();
+  }
+
+  // Change in astah: room -> newRoom
+  public void changeRoom(Room newRoom)
+  {
+    this.room = newRoom.copy();
+  }
+
+  // Change in astah: dateTime -> newDateTime
+  public void changeStartTime(DateTime newDateTime)
+  {
+    this.dateTime = newDateTime.copy();
+  }
+
+  // toString + equals might not be needed?
+
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof Session))
+    {
+      return false;
+    }
+    Session other = (Session) obj;
+    return course.equals(other.course) && length == other.length
+        && dateTime.equals(other.dateTime) && room.equals(other.room);
+  }
+
+  public String toString()
+  {
+    return "Course: " + course + ", Date: " + dateTime.getDate() + ", Time: "
+        + dateTime.getTime() + ", Room: " + room;
+  }
+}
---
+++ Sep1/SessionList.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class SessionList
+{
+  private ArrayList<Session> sessionList;
+
+  // ADD to astah
+  public SessionList()
+  {
+    this.sessionList = new ArrayList<>();
+  }
+
+  public void addSession(Session session)
+  {
+    sessionList.add(session);
+  }
+
+  // Should maybe be changed in Astah?? Se examples below:
+  public void removeSession(Session session)
+  {
+
+    /*
+    Could instead be something like:
+
+    removeSession(DateTime dateTime, String course)
+    {
+    sessionList.remove(getSessionByDateTimeAndCourse(dateTime, course));
+    }
+     */
+    sessionList.remove(session);
+  }
+
+  public Session getSession(Session session)
+  {
+    for (int i = 0; i < sessionList.size(); i++)
+    {
+      if (sessionList.get(i).equals(session))
+        return sessionList.get(i);
+    }
+
+    throw new NullPointerException("Session not found");
+  }
+
+  // Might not be useable in our case - By index
+  public Session getSessionByIndex(int index)
+  {
+    return sessionList.get(index);
+  }
+
+  // By TimeDate & Course - might be good?? - throws nullpointer-exception
+  public Session getSessionByDateTimeAndCourse(DateTime dateTime, String course)
+  {
+    for (int i = 0; i < sessionList.size(); i++)
+    {
+      if (sessionList.get(i).getCourse().equals(course) && sessionList.get(i)
+          .getDateTime().equals(dateTime))
+        return sessionList.get(i);
+    }
+
+    throw new NullPointerException("Session not found");
+  }
+
+}
